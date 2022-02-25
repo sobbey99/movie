056f1cd48d6d308241bc2697dad5c67744a8385f
sobbey99 <sobbey99@gmail.com>
2022-02-25 22:55:23 +0200

    Task 1 Done

--- script.js
+++ script.js
@@ -23,3 +23,21 @@
 
 'use strict';
 
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', ''),
+    personalMovieDB = {
+        count: numberOfFilms,
+        movies: {},
+        actors: {},
+        genres: [],
+        privat: false
+    }; 
+    
+
+    const a = prompt('Один из последних просмотренных фильмов?' ,'');
+    const b = +prompt('На сколько оцените его?' ,'');
+    const c = prompt('Один из последних просмотренных фильмов?' ,'');
+    const d = +prompt('На сколько оцените его?' ,'');
+
+    personalMovieDB.movies[a] = b;
+    personalMovieDB.movies[c] = d;
+
